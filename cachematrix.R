3d8c32ce2beb5ad55409ee37e5f3a4e3cedb5a15
Marouan <Marouan@MacBook-Air-de-Marouan.local>
2017-12-27 06:26:37 +0100

    R Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+#
 ## Write a short comment describing this function
+## l'objectif est de pouvoir mettre en cache le contenu d'une matrice. 
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+
+
+## -------------
+#  to Test the code : 
+#  source("cachematrix.R")
+#  myMatrix = makecache(matrix(0:10, 10, 10))
+# 
+
+
+
+makecache <- function(x = matrix()) {
+
+	# Initialisation du cache 
+	cache <- NULL 
+
+
+	setMatrix <- function(y) {
+    x <<- y
+    cache <<- NULL
+  }
+
+  getMatrix <- function() x
+  
+  getCache <- function() cache
+  
+  setCache <- function(inverse) cache <<- inverse
+
+  list(setMatrix = setMatrix, getMatrix = getMatrix, setCache = setCache, getCache = getCache)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+    cache <- x$getCache()
+         
+    if (!is.null(cache)) 
+    {
+    	message("Problem 201: The cache could not be loaded")
+    	return(cache)
+   } else {
+    	dMatrix <- x$getMatrix()
+    	cache <- solve(dMatrix, ...)
+    	x$setCache(cache)
+   		return(cache)
+  }
+  
 }
